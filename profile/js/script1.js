fd5ed76f04965d16612fc65a2f569c89fccc261d
BonierF <141430620+BonierF@users.noreply.github.com>
2023-09-25 11:09:13 -0300

    Add files via upload

---
+++ profile/js/script1.js
@@ -0,0 +1,8 @@
+function EnviarMensagem() {
+    var nome = document.getElementById("idNome");
+
+    if(nome.value != "") {
+        alert("obrigado, sr(a)" + nome.value + " , sua mensagem foi enviada com sucesso!");
+
+    }
+}
